71a9de535ca31e5a7b88d276fade87b9c0737fd1
Aashu-007 <gnirmal16@gmail.com>
2021-11-22 22:36:13 +0530

    Adding files

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Drawer from './Drawer'
+import Card from './Card'
+
+function App() {
+  return (
+    <div>
+      <Drawer>
+      <Card/>
+      </Drawer>
+    </div>
+  );
+}
+
+export default App;
